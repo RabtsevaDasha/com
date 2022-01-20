de10a9b903da00b73cc9afe9a4d74d9ce06e61b4
Dasha <dryabtseva05@gmail.com>
2022-01-20 20:12:35 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+        int num = 5;
+        int num2 = 6;
+
+        int result = num - num2;
+        System.out.println("Result: " + result);
+    }
+}
