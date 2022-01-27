1ae80963e912d80ff77c21e8c600cdbced017b25
Dasha <dryabtseva05@gmail.com>
2022-01-27 19:37:33 +0300

    initial commit

---
+++ src/task/Home2.java
@@ -0,0 +1,11 @@
+package task;
+public class Home2 {
+    public static void main(String[] args) {
+        int a = 1, b = 3, p = 0, i;
+
+        for (i =1; i < 5; i++){
+            p = p+a+b;
+            System.out.println("A*B = " + p);
+        }
+    }
+}
