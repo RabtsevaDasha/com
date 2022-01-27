8886a4cfdaf9b8e8d2260023bcf35d93d369c468
Dasha <dryabtseva05@gmail.com>
2022-01-27 21:03:39 +0300

    initial commit

--- src/task/home4.java
+++ src/task/home4.java
@@ -2,6 +2,10 @@ package task;
 
 public class home4 {
     public static void main(String[] args) {
-        System.out.println("df");
+        float distance = 10;
+        for (int day = 2; day <= 7; day++){
+            distance *= 1.1;
+            System.out.println("В день " + day + " спортсмен пробежал " + distance + " km");
+        }
     }
 }
