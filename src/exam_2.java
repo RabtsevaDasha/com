3fd013c2051feb7e23cecc3752b72a083a21a5fd
Dasha <dryabtseva05@gmail.com>
2022-01-30 20:12:50 +0300

    ex_sum

--- src/exam_2.java
+++ src/exam_2.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 
 public class exam_2 {
     public static void main(String[] args) {
-         System.out.println("Введите ваше число: ");
-         Scanner sc = new Scanner(System.in);
-         int num = sc.nextInt();
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 // //What did I say at the previous lesson? We could do it in 1 loop!
-         int sum1 = 0;
-         for (int i = 1; i < num; i++){
-             sum1+=i;
-        }
-         int sum2 = 0;
-         for (int i = 1; i < num; i+=2) {
-            sum1 += i;
-        }
-                System.out.println(sum1);
-                System.out.println(sum2);
+        int evenSum=0, oddSum=0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                evenSum += i;
+            } else oddSum += i;
+        } int sum=evenSum+oddSum;
+        System.out.println("Sum of even numbers: "+evenSum+"\nSum of odd numbers: "+oddSum+"\nTotal sum: "+sum);
     }
 }
