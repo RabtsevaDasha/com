21686f17df6f3709249e14eacc88064d8b854ca0
Dasha <dryabtseva05@gmail.com>
2022-01-31 19:19:21 +0300

    exam_calculator

--- src/Exam_1.java
+++ src/Exam_1.java
@@ -4,40 +4,38 @@ import java.util.Scanner;
 
 public class Exam_1 {
     public static void main(String[] args) {
-        System.out.println("Enter two numbers: ");
         Scanner sc = new Scanner(System.in);
         double num1;
         double num2;
         double answ;
         String op;
-        num1 = sc.nextDouble();
-        num2 = sc.nextDouble();
-        System.out.print("\nEnter an operator (+, -, *, /): ");
-        op = sc.next();
-        //use text formatting
-        switch (op){
-            case "+": answ = num1 + num2; break;
-            case "-": answ = num1 - num2; break;
-            case "*": answ = num1 * num2; break;
+        String ans2;
+        do {
+            System.out.print("Enter number 1: ");
+            num1 = sc.nextDouble();
+            System.out.print("Enter number 2: ");
+            num2 = sc.nextDouble();
+            System.out.print("Enter an operator (+, -, *, /): ");
+            op = sc.next();
+            //use text formatting
+            switch (op) {
+                case "+": answ = num1 + num2; break;
+                case "-": answ = num1 - num2; break;
+                case "*": answ = num1 * num2; break;
                 //if we divide 2 int numbers, then we will lose some data. Use double instead. And what if num2 is 0?
-            case "/": answ = num1 / num2; break;
-            default:  System.out.print("Error! Enter correct operator");
-                return;
-        }
-        System.out.print("\nThe result is given as follows:\n");
-        System.out.print(num1 + " " + op + " " + num2 + " = " + answ);
-          //you do not have a loop! Your's program will just finish, but I need it to work till I enter stop word
-        System.out.println("\nDo you want to continue?\n");
-        String ans2 = sc.nextLine();
-        System.out.println(ans2);
-        op = sc.nextLine();
+                case "/": answ = num1 / num2; break;
+                default:
+                    System.out.print("Error! Enter correct operator");
+                    return;
+            }
+            System.out.println("\nThe result is given as follows:");
+            System.out.println(num1 + " " + op + " " + num2 + " = " + answ);
+            //you do not have a loop! Your's program will just finish, but I need it to work till I enter stop word
+            System.out.println("Do you want to continue?");
+            ans2 = sc.next();
 //ans2.equalsIgnoreCase(...). What if I enter "hello" instead of "yes"/"no"?
-        if(ans2.contains("Yes")|| ans2.contains("yes")){
-//did you test your program? You do not have a loop to repeat. You will be just cut off and the program will stop. 
-            return;
-        }
-        if(ans2.contains("No") || ans2.contains("no")){
-            System.out.println("You are done calculating!");
+//did you test your program? You do not have a loop to repeat. You will be just cut off and the program will stop.
+             } while (ans2.toLowerCase().contains("yes"));
+        System.out.println("You are done calculating!");
         }
     }
-}
